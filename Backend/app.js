868f90dfae94038a6cb563dc523058f2e14372a6
Om-Lachake <omklachake@gmail.com>
2024-12-07 16:44:17 +0530

    Create app.js

---
+++ Backend/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+const cors = require('cors');
+const connectToDb = require('./db/db');
+
+
+connectToDb();
+app.use(cors())
+app.get('/', (req, res) => {
+    res.send("UBER")
+})
+
+module.exports = app;
